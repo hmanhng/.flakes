434142a19b81f8d70ee1acfbf776b75fcafd75db
Hmanhng <hmanhng@icloud.com>
2023-06-10 21:44:28 +0700

    enable DRM firefox

--- home/packages/common/firefox/default.nix
+++ home/packages/common/firefox/default.nix
@@ -23,11 +23,11 @@
 
         DisplayBookmarksToolbar = true;
         Preferences = {
-          "browser.toolbars.bookmarks.visibility" = "always";
           "browser.tabs.inTitlebar" = 0;
           "browser.aboutConfig.showWarning" = false;
           "toolkit.legacyUserProfileCustomizations.stylesheets" = true;
           "media.ffmpeg.vaapi.enabled" = true;
+          "media.eme.enabled" = true; # Enable DRM
           "extensions.autoDisableScopes" = 0;
         };
       };
@@ -75,6 +75,7 @@
         };
       };
       settings = {
+        "browser.toolbars.bookmarks.visibility" = "always"; # Show bookmarks
         "browser.startup.homepage" = "file://${./homepage.html}";
         "extensions.activeThemeID" = "firefox-compact-light@mozilla.org";
       };
