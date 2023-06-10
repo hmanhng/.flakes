6b0a22a0079937e72ee3cda791ab60b773eb7f32
Hmanhng <hmanhng@icloud.com>
2023-06-10 23:17:12 +0700

    somechange

--- home/packages/common/mpv/default.nix
+++ home/packages/common/mpv/default.nix
@@ -1,10 +1,8 @@
 { lib, pkgs, user, ... }:
 
 {
-  programs = {
-    mpv = {
-      enable = true;
-    };
+  programs.mpv = {
+    enable = true;
   };
   home.file.".config/mpv/mpv.conf".source = ./mpv.conf;
   home.file.".config/mpv/scripts/file-browser.lua".source = ./scripts/file-browser.lua;
--- home/packages/common/search/default.nix
+++ home/packages/common/search/default.nix
@@ -4,12 +4,20 @@
   home = {
     packages = with pkgs; [
       fd
-      bat
       ripgrep
     ];
   };
-  programs = {
-    fzf.enable = true;
+  programs.fzf.enable = true;
+  programs.bat = {
+    enable = true;
+    themes = {
+      Catppuccin-macchiato = builtins.readFile (builtins.fetchurl {
+        url = "https://raw.githubusercontent.com/catppuccin/bat/main/Catppuccin-macchiato.tmTheme";
+        sha256 = "sha256:0ydk3ysvxgdy829jfl2bwhzhwv3kh5d4k2l6047bi8dwbbid59zg";
+      });
+    };
+    config = {
+      theme = "Catppuccin-macchiato";
+    };
   };
-  xdg.configFile."bat/themes".source = ./bat;
 }
