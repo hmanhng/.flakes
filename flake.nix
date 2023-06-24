06d0de1688d6106fdc54233a011627e1e24880d8
Hmanhng <hmanhng@icloud.com>
2023-06-24 16:55:22 +0700

    test github actions.

--- flake.nix
+++ flake.nix
@@ -65,13 +65,13 @@
       "https://nix-community.cachix.org"
       "https://hyprland.cachix.org"
       # "https://helix.cachix.org"
-      # "https://fufexan.cachix.org"
+      "https://hmanhng.cachix.org"
     ];
     extra-trusted-public-keys = [
       "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
       "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
       # "helix.cachix.org-1:ejp9KQpR1FBI2onstMQ34yogDm4OgU2ru6lIwPvuCVs="
-      # "fufexan.cachix.org-1:LwCDjCJNJQf5XD2BV+yamQIMZfcKWR9ISIFy5curUsY="
+      "hmanhng.cachix.org-1:+pXFpN2CtS0rNUdCdeiOu6QUWMVBX0nCbWREhfiiKtI="
     ];
   };
 }
