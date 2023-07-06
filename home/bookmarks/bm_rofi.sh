f4aa8bb34d4d3bb7ea8e9eec1289703fd30e1589
Hmanhng <hmanhng@icloud.com>
2023-07-07 02:57:47 +0700

    update secret

--- home/bookmarks/bm_rofi.sh
+++ home/bookmarks/bm_rofi.sh
@@ -3,4 +3,4 @@
 bookmarks=~/.flakes/home/bookmarks/default
 rofi="rofi -dmenu -theme ~/.config/rofi/cliphist/cliphist-rofi.rasi -p Bookmarks"
 
-ydotool type "$(grep -v "^#" $bookmarks | $rofi)"
+ydotool type -d 1 "$(grep -v "^#" $bookmarks | $rofi)"
