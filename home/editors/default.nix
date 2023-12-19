72e91c51e6e2a094ec946a05bccac7410c80e532
Hmanhng <hmanhng@icloud.com>
2023-12-20 02:12:40 +0700

    feat(eclipse): add eclipse for learn java

--- home/editors/default.nix
+++ home/editors/default.nix
@@ -2,11 +2,23 @@
   pkgs,
   default,
   ...
-}: {
+}: let
+  jdtls =
+    pkgs.runCommand "jdtls" {
+      buildInputs = [pkgs.makeWrapper];
+    } ''
+      mkdir $out
+      ln -s ${pkgs.jdt-language-server}/* $out
+      rm $out/bin
+      mkdir $out/bin
+      ln -s ${pkgs.jdt-language-server}/bin/jdt-language-server $out/bin/jdtls
+    '';
+in {
   imports = [
     ./code
     ./nvim
     ./emacs
+    ./eclipse
     # ./jetbrains
   ];
   home = {
@@ -21,6 +33,7 @@
     nil
     alejandra
 
+    jdtls
     # texlive.combined.scheme-medium
     # required by +jupyter
     # (python3.withPackages (ps: with ps; [xdg]))
---
+++ home/editors/eclipse/default.nix
@@ -0,0 +1,3 @@
+{pkgs, ...}: {
+  home.packages = with pkgs; [(eclipses.override {jdk = jdk21;}).eclipse-java];
+}
