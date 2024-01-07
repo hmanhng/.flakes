ae5140bd3c536988b16de1166695996a60cd8c17
Hmanhng <hmanhng@icloud.com>
2024-01-08 03:50:13 +0700

    fix(nvim): comment

--- home/editors/nvim/lua/plugins/disable.lua
+++ home/editors/nvim/lua/plugins/disable.lua
@@ -1,5 +1,5 @@
 return {
   { "rcarriga/nvim-notify",          enabled = false },
-  { "echasnovski/mini.comment",      enabled = false },
+  --[[ { "echasnovski/mini.comment",      enabled = false }, ]]
   { "nvim-telescope/telescope.nvim", enabled = false },
 }
