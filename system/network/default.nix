103616c91bd2880081084ede306fcc352839127b
Hmanhng <hmanhng@icloud.com>
2024-03-01 01:55:52 +0700

    feat(network): add v2raya

--- system/network/default.nix
+++ system/network/default.nix
@@ -18,6 +18,8 @@
     resolved.enable = true;
   };
 
+  services.v2raya.enable = true;
+
   # Don't wait for network startup
   # systemd.services.NetworkManager-wait-online.enable = lib.mkForce false;
 }
