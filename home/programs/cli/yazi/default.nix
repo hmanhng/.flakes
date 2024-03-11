2d0a1ebd8140a8cf11aa41470418431acabb7ce6
Hmanhng <hmanhng@icloud.com>
2024-03-12 01:20:22 +0700

    tweak(yazi): fix key

--- home/programs/cli/yazi/default.nix
+++ home/programs/cli/yazi/default.nix
@@ -5,11 +5,6 @@
     enableFishIntegration = true;
   };
 
-  home.file = {
-    ".config/yazi/yazi.toml".source = ./yazi.toml;
-    ".config/yazi/keymap.toml".source = ./keymap.toml;
-    ".config/yazi/theme.toml".source = ./theme.toml;
-  };
   home.packages = with pkgs; [
     exiftool
     mediainfo
