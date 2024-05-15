38b802656dd14ab009e3f48a4b883a78b817c7e2
Hmanhng <hmanhng@icloud.com>
2024-05-16 06:48:40 +0700

    tweak(v2raya): disable

--- system/network/default.nix
+++ system/network/default.nix
@@ -18,7 +18,7 @@
     resolved.enable = true;
   };
 
-  services.v2raya.enable = true;
+  # services.v2raya.enable = true;
 
   # Don't wait for network startup
   # systemd.services.NetworkManager-wait-online.enable = lib.mkForce false;
