610d0b5bf6f499788a03bde3b6e21d6f8743a23b
Hmanhng <hmanhng@icloud.com>
2024-05-16 06:48:23 +0700

    fix(bluez): cant connect keyboard

--- system/hardware/bluetooth.nix
+++ system/hardware/bluetooth.nix
@@ -3,13 +3,7 @@
     enable = true;
     package = pkgs.bluez5-experimental;
     settings = {
-      # make Xbox Series X controller work
       General = {
-        Class = "0x000100";
-        ControllerMode = "bredr";
-        FastConnectable = true;
-        JustWorksRepairing = "always";
-        Privacy = "device";
         # Battery info for Bluetooth devices
         Experimental = true;
       };
