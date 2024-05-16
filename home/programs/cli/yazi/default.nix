181038816ca0f2550e12b69dc00181407cf78e8a
Hmanhng <hmanhng@icloud.com>
2024-05-17 05:15:08 +0700

    tweak(yazi): add config

--- home/programs/cli/yazi/default.nix
+++ home/programs/cli/yazi/default.nix
@@ -1,8 +1,31 @@
-{pkgs, ...}: {
+{
+  pkgs,
+  config,
+  ...
+}: {
   programs.yazi = {
     enable = true;
     # package = inputs.yazi.packages.${pkgs.system}.yazi;
     enableFishIntegration = true;
+    settings = {
+      manager = {
+        layout = [1 4 3];
+        sort_by = "alphabetical";
+        sort_sensitive = true;
+        sort_reverse = false;
+        sort_dir_first = true;
+        linemode = "none";
+        show_hidden = false;
+        show_symlink = true;
+      };
+
+      preview = {
+        tab_size = 2;
+        max_width = 600;
+        max_height = 900;
+        cache_dir = config.xdg.cacheHome;
+      };
+    };
   };
 
   home.packages = with pkgs; [
