5fb44de8db549923f9b15245a6f3eb382008d04f
Hmanhng <hmanhng@icloud.com>
2024-05-25 14:27:48 +0700

    tweak(thorium-browser): update to 124.0.6367.218

--- pkgs/thorium-browser/default.nix
+++ pkgs/thorium-browser/default.nix
@@ -2,152 +2,26 @@
   lib,
   stdenv,
   fetchurl,
-  autoPatchelfHook,
-  dpkg,
-  wrapGAppsHook,
-  alsa-lib,
-  at-spi2-atk,
-  at-spi2-core,
-  cairo,
-  cups,
-  curl,
-  dbus,
-  expat,
-  ffmpeg,
-  fontconfig,
-  freetype,
-  glib,
-  glibc,
-  gtk3,
-  gtk4,
-  libcanberra,
-  liberation_ttf,
-  libexif,
-  libglvnd,
-  libkrb5,
-  libnotify,
-  libpulseaudio,
-  libu2f-host,
-  libva,
-  libxkbcommon,
-  mesa,
-  nspr,
-  nss,
-  pango,
-  pciutils,
-  pipewire,
-  qt6,
-  speechd,
-  udev,
-  _7zz,
-  vaapiVdpau,
-  vulkan-loader,
-  wayland,
-  wget,
-  xdg-utils,
-  xfce,
-  xorg,
+  appimageTools,
 }:
-stdenv.mkDerivation rec {
+appimageTools.wrapType2 rec {
   pname = "thorium-browser";
-  version = "119.0.6045.214";
+  version = "124.0.6367.218";
 
   src = fetchurl {
-    url = "https://github.com/Alex313031/thorium/releases/download/M${version}/thorium-browser_${version}_amd64.deb";
-    hash = "sha256-Xk9f+zV64327PcPtLaRs1HeBA3081vPLWdd1cnJPjlo=";
+    url = "https://github.com/Alex313031/thorium/releases/download/M${version}/Thorium_Browser_${version}_AVX.AppImage";
+    hash = "sha256-xwFB5uPNAaM9JhulJHIdKgWuo+eO0CGCBqUupcs4rnA=";
   };
-
-  nativeBuildInputs = [
-    autoPatchelfHook
-    dpkg
-    wrapGAppsHook
-    qt6.wrapQtAppsHook
-  ];
-
-  buildInputs = [
-    stdenv.cc.cc.lib
-    alsa-lib
-    at-spi2-atk
-    at-spi2-core
-    cairo
-    cups
-    curl
-    dbus
-    expat
-    ffmpeg
-    fontconfig
-    freetype
-    glib
-    glibc
-    gtk3
-    gtk4
-    libcanberra
-    liberation_ttf
-    libexif
-    libglvnd
-    libkrb5
-    libnotify
-    libpulseaudio
-    libu2f-host
-    libva
-    libxkbcommon
-    mesa
-    nspr
-    nss
-    qt6.qtbase
-    pango
-    pciutils
-    pipewire
-    speechd
-    udev
-    _7zz
-    vaapiVdpau
-    vulkan-loader
-    wayland
-    wget
-    xdg-utils
-    xfce.exo
-    xorg.libxcb
-    xorg.libX11
-    xorg.libXcursor
-    xorg.libXcomposite
-    xorg.libXdamage
-    xorg.libXext
-    xorg.libXfixes
-    xorg.libXi
-    xorg.libXrandr
-    xorg.libXrender
-    xorg.libXtst
-    xorg.libXxf86vm
-  ];
-
-  autoPatchelfIgnoreMissingDeps = [
-    "libQt5Widgets.so.5"
-    "libQt5Gui.so.5"
-    "libQt5Core.so.5"
-  ];
-
-  installPhase = ''
-    runHook preInstall
-    mkdir -p $out
-    cp -vr usr/* $out
-    cp -vr etc $out
-    cp -vr opt $out
-    ln -sf $out/opt/chromium.org/thorium/thorium-browser $out/bin/thorium-browser
-    substituteInPlace $out/share/applications/thorium-shell.desktop \
-      --replace /usr/bin $out/bin \
-      --replace /opt $out/opt
+  extraInstallCommands = let
+    contents = appimageTools.extract {inherit pname version src;};
+  in ''
+    install -Dm444 ${contents}/thorium-browser.desktop $out/share/applications/thorium-browser.desktop
+    install -Dm444 ${contents}/thorium.png $out/share/icons/hicolor/512x512/apps/thorium.png
     substituteInPlace $out/share/applications/thorium-browser.desktop \
       --replace /usr/bin $out/bin \
       --replace StartupWMClass=thorium StartupWMClass=thorium-browser \
-      --replace Icon=thorium-browser Icon=$out/opt/chromium.org/thorium/product_logo_256.png
-    addAutoPatchelfSearchPath $out/chromium.org/thorium
-    addAutoPatchelfSearchPath $out/chromium.org/thorium/lib
-    substituteInPlace $out/opt/chromium.org/thorium/thorium-browser \
-      --replace 'export LD_LIBRARY_PATH' "export LD_LIBRARY_PATH=\$LD_LIBRARY_PATH:${lib.makeLibraryPath buildInputs}:$out/chromium.org/thorium:$out/chromium.org/thorium/lib"
-    makeWrapper "$out/opt/chromium.org/thorium/thorium-browser" "$out/bin/thorium-browser" \
-      --add-flags "\''${NIXOS_OZONE_WL:+\''${WAYLAND_DISPLAY:+--ozone-platform-hint=auto --enable-features=WaylandWindowDecorations}}"
-    runHook postInstall
+      --replace Icon=thorium Icon=$out/share/icons/hicolor/512x512/apps/thorium.png \
+      --replace Exec=thorium Exec=$out/bin/thorium-browser
   '';
 
   meta = with lib; {
