b6eaff56330219e80594874b10d2849b8623c4f0
Hmanhng <hmanhng@icloud.com>
2024-12-13 00:25:16 +0700

    home/hyprlock: redesign

--- home/programs/wayland/hyprlock.nix
+++ home/programs/wayland/hyprlock.nix
@@ -3,17 +3,16 @@
   inputs,
   pkgs,
   ...
-}: let
-  font_family = "Maple Mono";
-in {
+}: {
   programs.hyprlock = {
     enable = true;
 
-    package = inputs.hyprlock.packages.${pkgs.system}.default;
+    package = inputs.hyprlock.packages.${pkgs.system}.hyprlock;
 
     settings = {
       general = {
         disable_loading_bar = true;
+        immediate_render = true;
         hide_cursor = false;
         no_fade_in = true;
       };
@@ -22,10 +21,6 @@ in {
         {
           monitor = "";
           path = config.theme.wallpaper;
-          blur_passes = 3;
-          brightness = 0.817200;
-          contrast = 0.891600;
-          vibrancy = 0.169600;
         }
       ];
 
@@ -33,22 +28,28 @@ in {
         {
           monitor = "eDP-1";
 
-          size = "350, 50";
+          size = "300, 50";
+          valign = "bottom";
+          position = "0%, 10%";
 
           outline_thickness = 1;
 
-          outer_color = "rgb(b6c4ff)";
-          inner_color = "rgb(dce1ff)";
-          font_color = "rgb(354479)";
+          font_color = "rgb(b6c4ff)";
+          outer_color = "rgba(180, 180, 180, 0.5)";
+          inner_color = "rgba(200, 200, 200, 0.1)";
+          check_color = "rgba(247, 193, 19, 0.5)";
+          fail_color = "rgba(255, 106, 134, 0.5)";
 
           fade_on_empty = false;
-          placeholder_text = ''<span font_family="${font_family}" foreground="##354479">Password...</span>'';
+          placeholder_text = "Enter Password";
 
           dots_spacing = 0.2;
           dots_center = true;
-          valign = "center";
-          halign = "center";
-          position = "0, -120";
+          dots_fade_time = 100;
+
+          shadow_color = "rgba(0, 0, 0, 0.1)";
+          shadow_size = 7;
+          shadow_passes = 2;
         }
       ];
 
@@ -56,26 +57,34 @@ in {
         {
           monitor = "";
           text = "$TIME";
-          inherit font_family;
           font_size = 150;
           color = "rgb(b6c4ff)";
 
-          position = "0, -300";
-
-          valign = "top";
-          halign = "center";
-        }
-        {
-          monitor = "";
-          text = "Hi <span foreground='##86469C'><i>$USER</i></span> :)";
-          inherit font_family;
-          font_size = 30;
-          color = "rgb(b6c4ff)";
-
-          position = "0, -40";
+          position = "0%, 30%";
 
           valign = "center";
           halign = "center";
+
+          shadow_color = "rgba(0, 0, 0, 0.1)";
+          shadow_size = 20;
+          shadow_passes = 2;
+          shadow_boost = 0.3;
+        }
+        {
+          monitor = "";
+          text = "cmd[update:3600000] date +'%a %b %d'";
+          font_size = 20;
+          color = "rgb(b6c4ff)";
+
+          position = "0%, 40%";
+
+          valign = "center";
+          halign = "center";
+
+          shadow_color = "rgba(0, 0, 0, 0.1)";
+          shadow_size = 20;
+          shadow_passes = 2;
+          shadow_boost = 0.3;
         }
       ];
     };
