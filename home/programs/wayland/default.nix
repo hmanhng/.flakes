463109b1061dbf94bed1ca1ee9ec5ce5a216a4c3
Hmanhng <hmanhng@icloud.com>
2025-02-17 20:07:46 +0700

    fix(xdg-desktop-portal-hyprland): fix service systemd

--- home/programs/wayland/default.nix
+++ home/programs/wayland/default.nix
@@ -32,8 +32,10 @@
 
   # make stuff work on wayland
   home.sessionVariables = {
+    GDK_BACKEND = "wayland";
     QT_QPA_PLATFORM = "wayland";
     SDL_VIDEODRIVER = "wayland";
+    CLUTTER_BACKEND = "wayland";
     XDG_SESSION_TYPE = "wayland";
   };
 
