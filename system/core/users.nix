81a4c9faba1f691e848b15d7c8383ed525462def
hmanhng <hmanhng@icloud.com>
2026-03-17 16:12:43 +0700

    fish: default shell

--- system/core/users.nix
+++ system/core/users.nix
@@ -6,28 +6,27 @@
 {
   # Change default shell to fish
   # https://nixos.wiki/wiki/Fish#Setting_fish_as_your_shell
-  # programs.fish = {
-  #   enable = true;
-  #   vendor.functions.enable = false;
-  #   vendor.config.enable = false;
-  # };
-  # users.defaultUserShell = pkgs.fish;
-  # environment.shells = with pkgs; [fish];
+  programs.fish = {
+    enable = true;
+    vendor.functions.enable = false;
+    vendor.config.enable = false;
+  };
+  environment.shells = with pkgs; [ fish ];
 
   users.users.root.initialHashedPassword = "$6$X8/X.cOzzY2d.ak9$VyhhkfKFgxFiKfbPQt6AzkL3duOr43B.O27N6eJy07tgZvOyzdygARwXv7R2dXBOegrTk2F.N7NC9RkBi/sff0";
 
   users.users.hmanhng = {
     isNormalUser = true;
-    shell = pkgs.bash;
+    shell = pkgs.fish;
     initialHashedPassword = config.users.users.root.initialHashedPassword;
     extraGroups = [
-      "wheel"
-      "video"
-      "audio"
-      "networkmanager"
       "input"
       "libvirtd"
+      "networkmanager"
       "plugdev"
+      "transmission"
+      "video"
+      "wheel"
     ];
   };
 
